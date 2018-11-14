1e4452476523f9371c5a944997154d393f9704a9
Kyle Troyer <ktroyer10@gmail.com>
2018-11-14 10:52:06 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  model.swift
+//  Todoey
+//
+//  Created by Kyle Troyer on 11/13/18.
+//  Copyright © 2018 Kyle Troyer. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var done: Bool = false
+    var title: String = ""
+    
+}
