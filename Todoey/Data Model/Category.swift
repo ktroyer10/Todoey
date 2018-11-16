5887a80bcd62a6ae814fa6a235f38e870cfcdc28
Kyle Troyer <ktroyer10@gmail.com>
2018-11-15 21:39:00 -0400

    Moved data models over to Realm infrastructure

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Kyle Troyer on 11/15/18.
+//  Copyright © 2018 Kyle Troyer. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category : Object {
+    @objc dynamic var name : String = ""
+    let items = List<Item>()
+}
+
